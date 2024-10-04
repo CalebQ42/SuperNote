9f1812d9bc358f90fefbff4c41493e65df8b4615
Caleb Gardner <calebq23@gmail.com>
2024-10-04 06:48:56 -0500

    More UI Web UI

--- lib/ui/infinite_canvas.dart
+++ lib/ui/infinite_canvas.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
 
-class InfiniteCanvas extends StatefulWidget{
+class InfiniteCanvas extends StatefulWidget {
+  final List<Widget> Function((int, int)) getWidgets;
+
+  const InfiniteCanvas({super.key, required this.getWidgets});
+
+  @override
+  State<StatefulWidget> createState() => _InfiniteCanvasState();
 }
 
-class _InfiniteCanvasState extends State<InfiniteCanvas>{
+class _InfiniteCanvasState extends State<InfiniteCanvas> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
 }
